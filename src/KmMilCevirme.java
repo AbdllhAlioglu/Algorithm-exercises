bc1752a9b9a066aa1ceb6f8f5dec38934f019567
Abdullah <abdullah.alioglu@bahcesehir.edu.tr>
2022-05-11 16:46:43 +0300

    km-mil çevirme
    3.bölüm

---
+++ src/KmMilCevirme.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class KmMilCevirme {
+    static void kmMilCevirme(double a){
+        double mil = (a * 0.621);
+        System.out.println("Mil Uzunluğu = " + mil + " mil");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Km giriniz = ");
+        double  km = scn.nextDouble();
+        kmMilCevirme(km);
+
+
+
+    }
+}
