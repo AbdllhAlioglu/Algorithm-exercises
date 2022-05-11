28a6497987c5d47f97bb769a3bad2f478ab94f7a
Abdullah <abdullah.alioglu@bahcesehir.edu.tr>
2022-05-11 16:44:27 +0300

    Update CevreAlanHesaplama.java

--- src/CevreAlanHesaplama.java
+++ src/CevreAlanHesaplama.java
@@ -1,6 +1,35 @@
-public class CevreAlanHesaplama {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class CevreAlanHesaplama {
+
+    static void kareCevre(double a) {
+        double cevre = 4 * a;
+        System.out.println("Çevre = " + cevre);
     }
 
+    static void kareAlan(double a) {
+        double alan = Math.pow(a, 1);
+        System.out.println("Alan = " + alan);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Karenin kenar uzunluğnu girin");
+
+        double kenar = scn.nextInt();
+
+        System.out.println("1 Alan\n2 Çevre");
+
+        int islem1 = scn.nextInt();
+
+        if (islem1 == 1) {
+            kareAlan(kenar);
+        } else if (islem1 == 2) {
+            kareCevre(kenar);
+        }
+
+
+    }
 }
