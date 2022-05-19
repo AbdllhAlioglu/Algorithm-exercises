b5f14e09ccc5a74bc585db0bfc4f74631f3fcd7a
Abdullah <abdullah.alioglu@bahcesehir.edu.tr>
2022-05-19 15:38:50 +0300

    Create IkiSayiKarsilastirma.java

---
+++ src/IkiSayiKarsilastirma.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class IkiSayiKarsilastirma {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("İki tane sayı giriniz : ");
+
+        int s1 = scn.nextInt();
+        int s2 = scn.nextInt();
+
+        System.out.println("1.Sayı =" + s1 +" --- "+ " 2.sayı =" +s2);
+
+        if (s1<s2){
+            System.out.println("2. sayı büyüktür");
+        }
+        else if (s1>s2){
+            System.out.println("1. sayı büyüktür");
+        }
+        else
+            System.out.println("İki sayı eşittir");
+
+
+    }
+
+
+}
